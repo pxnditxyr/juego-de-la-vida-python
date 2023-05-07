32554665af416da60a1e14a4e37e383b8901e136
Yuki xD <pxnditxyr@gmail.com>
2023-05-06 20:42:57 -0400

    feat: added rule 11 of wolfram

--- generar_reglas.py
+++ generar_reglas.py
@@ -34,3 +34,19 @@ def generar_regla_1(filas_matriz: int, columnas_matriz:int):
                 if mitad_matriz !=(j-1) and mitad_matriz !=(j+1) and mitad_matriz!=j:
                     matriz[i][j]=1     
     return matriz
+
+def generar_regla_11 ( filas_matriz : int, columnas_matriz : int ):
+    matriz = generar_matriz( filas_matriz, columnas_matriz )
+    mitad_matriz = ( columnas_matriz - 1 ) // 2 if columnas_matriz % 2 == 0 else columnas_matriz // 2
+    pibote = mitad_matriz
+    matriz[ 0 ][ mitad_matriz ] = 1
+    for i in range( 1, filas_matriz ):
+        for j in range( columnas_matriz ):
+            if i % 2 == 0:
+                if j >= pibote and j <= pibote + 1:
+                    matriz[ i ][ j ] = 1
+            else:
+                if j < pibote or j > pibote + 1:
+                    matriz[ i ][ j ] = 1
+        pibote += 1
+    return matriz
